b1343727c71de3983fef3adfcaa183ccf73f4010
KevinTrico <kevin.colon24@upr.edu>
2026-09-17 19:22:22 -0400

    Added Partner A Name and stuff

--- Git-GitHub-Workshop-1/app.js
+++ Git-GitHub-Workshop-1/app.js
@@ -13,10 +13,10 @@ const TEAM_INFO = {
 
 // PARTNER A TASK: Replace only the values in this object with Partner A's details.
 const PARTNER_A = {
-  name: "Partner A",
-  major: "Your major",
+  name: "Kevin",
+  major: "Computer Engineering",
   favoriteTech: "HTML",
-  initials: "A",
+  initials: "K.C.T.",
 };
 
 // PARTNER B TASK: Replace only the values in this object with Partner B's details.
